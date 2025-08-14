675b643a93c9213e703935d28984ab526744d247
Mert <72713042+onemanitarmy@users.noreply.github.com>
2025-08-14 13:45:14 +0200

    Add files via upload

---
+++ terraformSetup/variables.tf
@@ -0,0 +1,7 @@
+# Example Variable of the location within Terraform.
+variable "location" {
+  description = "The Azure region where resources will be created."
+  type        = string
+  default     = "westeurope"
+
+}
